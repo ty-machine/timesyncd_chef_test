b72f6de3da4c324fc69858efa76eaeaa5589d64f
Ty Schreiner <t.schreiner@m1finance.com>
2020-04-15 16:53:41 -0500

    apply and reload

--- cookbooks/sysd-test/attributes/default.rb
+++ cookbooks/sysd-test/attributes/default.rb
@@ -1,2 +1,4 @@
 #update ntp to use gov required timeserver
-override['systemd_timesyncd']['time_ntp'] = ['time.nist.gov']
+#override['systemd_timesyncd']['time_ntp'] = ['time.nist.gov']
+#override['systemd_timesyncd']['time']['ntp'] = ['time.nist.gov']
+override['systemd']['time'] = ['time.nist.gov']
--- cookbooks/sysd-test/recipes/default.rb
+++ cookbooks/sysd-test/recipes/default.rb
@@ -1,1 +1,8 @@
 include_recipe 'systemd::timesyncd'
+
+systemd_timesyncd 'my-overrides' do
+  time do
+    ntp ['time.nist.gov']
+  end
+  notifies :restart, "service[systemd-timesyncd]"
+end
